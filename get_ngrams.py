367d4ced8514e826cdc2fc68acaf15dca9473958
anushreeapk <anushree.apk@gmail.com>
2018-01-19 13:06:30 -0500

    Made iterable version of ngrams generation using bigrams

--- get_ngrams.py
+++ get_ngrams.py
@@ -1,5 +1,5 @@
-import operator
-f = open('text.done.data.accLangDB-wsj_train.transcription.it6','r')
+import operator,re
+f = open('etc/text.done.data.it8','r')
 data = f.readlines()
 bigrams = {}
 trigrams = {}
@@ -31,14 +31,35 @@ for line in data :
 
 #print bigrams
 sorted_x = sorted(bigrams.items(), key=operator.itemgetter(1),reverse=True)
-#print sorted_x[:50]
-sorted_y = sorted(trigrams.items(), key=operator.itemgetter(1),reverse=True)
-
+top_bigrams = sorted_x[:50]
+#print top_bigrams
+#line = top_bigrams[0]
+#print line
+#print 'AH' in line[0]
+#sorted_y = sorted(trigrams.items(), key=operator.itemgetter(1),reverse=True)
+#s = raw_input()
+#Make changes to the text.done.data file
+h = open('etc/text.done.data.it8','w')
+for line in data:
+	phonemes = line
+	line = line.split("\"")
+	words = line[1].split()
+	for w in range(len(words)-1):
+		for bigram in top_bigrams:
+			if words[w] in bigram[0][0]:
+				if words[w+1] in bigram[0][1]:
+					#print bigram[0]
+					phonemes = phonemes.replace(words[w]+" "+words[w+1],words[w]+'_'+words[w+1])
+					
+	h.write(phonemes)	
+print " ngrams written to text.done.data"			
+					#s=raw_input()
 #Writing to bigram addenda file
-g = open('cmu_us_ceb_addenda_bigram.scm','w')
-j = open('cmu_us_ceb_addenda_trigram.scm','w')
+h.close()
+g = open('festvox/cmu_us_ceb_addenda.scm','w')
+#j = open('cmu_us_ceb_addenda_trigram.scm','w')
 ndata = []
-for bi in sorted_x:
+for bi in top_bigrams:
 		#(('AH', 'N'), 5331)
 		bi = list(bi)
 		pair = '_'.join(bi[0])
@@ -49,12 +70,26 @@ for bi in sorted_x:
         #        n_line ="(lex.add.entry '("+pair+" nil (((pau) 0))))"
         #else:
 		n_line ="(lex.add.entry '("+pair+" nil ((("+phone+") 0))"
-		print n_line
+		#print n_line
 		g.write(n_line+'\n')
 
-print "Bigrams : ", len(bigrams.keys())
-s = raw_input()
-for tri in sorted_y:
+print " ngram addenda file generated."
+
+#print "Bigrams : ", len(bigrams.keys())
+#s = raw_input()
+
+
+
+
+
+
+
+
+
+
+
+
+"""for tri in sorted_y:
 		#(('AH', 'N'), 5331)
 		tri = list(tri)
 		pair = '_'.join(tri[0])
@@ -69,3 +104,4 @@ for tri in sorted_y:
 		j.write(n_line+'\n')
 
 print "Trigrams : ", len(trigrams.keys())
+"""
